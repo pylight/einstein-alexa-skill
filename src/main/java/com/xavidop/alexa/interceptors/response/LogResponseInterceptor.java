1d390760294cf182450f6313fea46bfe90708e15
Sven Kammerer <sven@kammerer.email>
2023-04-29 12:40:44 +0200

    Switch to gradle and logback/slf4j

--- src/main/java/com/xavidop/alexa/interceptors/response/LogResponseInterceptor.java
+++ src/main/java/com/xavidop/alexa/interceptors/response/LogResponseInterceptor.java
@@ -3,14 +3,14 @@ package com.xavidop.alexa.interceptors.response;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.interceptor.ResponseInterceptor;
 import com.amazon.ask.model.Response;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import java.util.Optional;
 
 public class LogResponseInterceptor implements ResponseInterceptor {
 
-    static final Logger logger = LogManager.getLogger(LogResponseInterceptor.class);
+    private static final Logger logger = LoggerFactory.getLogger(LogResponseInterceptor.class);
     @Override
     public void process(HandlerInput input, Optional<Response> output) {
         logger.info(output.toString());
